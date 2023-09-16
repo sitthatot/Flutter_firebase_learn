526292bd512a13e27b8eccd6473ebdbe4ca269e6
sitthatot <sitthatot@gmail.com>
2023-09-16 22:52:41 +0700

    get realtime location and update database

--- lib/update_page.dart
+++ lib/update_page.dart
@@ -19,6 +19,8 @@ class _UpdatePageState extends State<UpdatePage> {
   late DatabaseReference dbRef;
   double latVal = 0.0;
   double longVal = 0.0;
+  late String lat;
+  late String long;
   Timer? timer;
   int count = 0;
   @override
@@ -31,7 +33,39 @@ class _UpdatePageState extends State<UpdatePage> {
       getdata();
       getRealData();
       getLocation();
+      _getCurrentLocation().then((value) {
+        lat = '${value.latitude}';
+        long = '${value.longitude}';
+        setState(() {
+          locationMessage = 'Latitude: $lat, Longitude: $long';
+        });
+        _liveLocation();
+        print(lat);
+      });
     });
+  }
+
+  String locationMessage = 'current Location of the User';
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location services are disabled.');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permission are permanently denied, we cannot request');
+    }
+
+    return await Geolocator.getCurrentPosition();
   }
 
   void getLocation() async {
@@ -44,6 +78,21 @@ class _UpdatePageState extends State<UpdatePage> {
     latVal = position.latitude;
     longVal = position.longitude;
     print(position.latitude);
+  }
+
+  void _liveLocation() {
+    LocationSettings locationSettings = const LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100,
+    );
+    Geolocator.getPositionStream(locationSettings: locationSettings)
+        .listen((Position position) {
+      lat = position.latitude.toString();
+      long = position.longitude.toString();
+    });
+    setState(() {
+      locationMessage = 'Latitude: $lat, Longitude: $long';
+    });
   }
 
   void getdata() {
@@ -78,12 +127,23 @@ class _UpdatePageState extends State<UpdatePage> {
             controller: longController,
             decoration: InputDecoration(label: Text("Long")),
           ),
-          Text(latVal.toString()),
-          Text(longVal.toString()),
+          Text(
+            locationMessage,
+            textAlign: TextAlign.center,
+          ),
           ElevatedButton(
               onPressed: () {
                 getRealData();
+                getLocation();
 
+                _getCurrentLocation().then((value) {
+                  lat = '${value.latitude}';
+                  long = '${value.longitude}';
+                  setState(() {
+                    locationMessage = 'Latitude: $lat, Longitude: $long';
+                  });
+                  _liveLocation();
+                });
                 // Map<String, String> user = {
                 //   'lat': latController.text,
                 //   'long': longController.text
